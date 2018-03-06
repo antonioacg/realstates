dc806ed115ba352edd75a9366cab6d4a46d2d2a8
Antonio Casagrande <antonioalves1706@gmail.com>
2018-03-06 04:35:46 -0300

    improve documentation

---
+++ src/modules/Authenticator/routes/_apidoc.ts
@@ -0,0 +1,8 @@
+/**
+ * @api {get} /login Get Authorization Bearer token for use with protected routes
+ * @apiName login
+ * @apiGroup Authentication
+ * @apiVersion  1.0.0
+ * @apiPermission Token
+ *
+ */
