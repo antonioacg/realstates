18bc2631cd431a6c1c849bb083ff6168ffbe66a8
Antonio Casagrande <antonioalves1706@gmail.com>
2018-03-06 04:27:42 -0300

    fix config

--- src/modules/Authenticator/routes/Authenticator-get.ts
+++ src/modules/Authenticator/routes/Authenticator-get.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import * as jwt from "jsonwebtoken";
-import config from "../../../config";
+
+import configDev from "../../../config/config-dev";
+import configProd from "../../../config/config-prod";
+
+// set environment
+const config = (process.env.NODE_ENV === "production"
+  ? configProd
+  : configDev);
 
 export const Login = (req: Request, res: Response, next: NextFunction): void => {
   const token = jwt.sign({}, config.jwtSecret);
